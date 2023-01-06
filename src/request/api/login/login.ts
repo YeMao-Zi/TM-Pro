d0d7188a4f0395c7b82617c05eeda42d169fa6c7
zhushengjie <1794541268@qq.com>
2023-01-06 11:50:41 +0800

    init

--- src/request/api/login/login.ts
+++ src/request/api/login/login.ts
@@ -1,7 +1,7 @@
 // b2b-店铺登录
 import { request } from "../../index";
 
-export function postManageLogin(data: any, complete?: Function) {
+export function postManageLogin(data: object, complete?: Function) {
 	return request({
 		method: "POST",
 		url: "/pb2b/miniShop/manage/login",
