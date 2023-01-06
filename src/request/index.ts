b01b18508d39171b2163c8996f8e89fc0cf46e55
zhushengjie <1794541268@qq.com>
2023-01-06 13:44:51 +0800

    init

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,5 +1,9 @@
 import { fetchNet } from "@/tmui/tool/lib/fetch";
 import { fetchConfig } from "@/tmui/tool/lib/interface";
+import useStore from "@/store"
+import { storeToRefs } from 'pinia'
+const { useInfoStore } = useStore()
+const { token } = storeToRefs(useInfoStore)
 
 export let baseUrl: string = ''; // 当为空时会判断是否是生产环境，否则就是写死的
 !baseUrl && (baseUrl = process.env.NODE_ENV === "development" ? "http://test.chuangjisu.com" : "https://ec.zjpuyule.com")
@@ -20,7 +24,6 @@ const whiteList = ["/captchaImage", "/login"];
 
 function beforeRequestFun(newConfig: fetchConfig) {
 	newConfig.url = baseUrl + newConfig.url;
-	const token = "xxxxxxx";
 	if (!whiteList.some((item) => newConfig.url && newConfig.url.includes(item))) {
 		newConfig.header = { ...newConfig.header, Authorization: token || "" };
 	}
