30aa9311901df67a31f95df14cb6e034ddfa6e0d
zhushengjie <1794541268@qq.com>
2023-01-11 18:00:05 +0800

    init

--- src/request/api/common/index.ts
+++ src/request/api/common/index.ts
@@ -1,17 +1,21 @@
 import { request } from "../../index";
 
-// complete可选，为执行成功后的回调相当于finally，其实直接用 .finally也可以,这里只是为了处理不兼容Promise的情况
+// 如果直接传入完整url,baseUrl会失效，即如果传入 url 为 http://xxx.com/login 不会再加上 baseUrl 了(环境判断也同样失效)
+// 若传入 /login ,会加上 url，输出为 baseUrl +  /login
+// complete 可选，为执行成功后的回调，相当于 finally，其实直接用 finally 也可以,这里只是保留了原生的 complete 能力
+// 无论 post 还是 get 请求统一传入 data:any 格式,不区分 body 传参还是 query 传参，已经做了封装
+
 export function getCaptchaImage(complete?: Function) {
 	return request({
 		method: "GET",
-		url: "/pyl/common/captchaImage",
+		url: "http://test.chuangjisu.com/pyl/common/captchaImage",
 	}, complete);
 }
 
-export function getSmsCaptcha(data: object, complete?: Function) {
+export function getSmsCaptcha(data: object) {
 	return request({
 		method: "GET",
 		url: "/pb2b/common/smsCaptcha",
 		data
-	}, complete);
+	});
 }
--- src/request/index.ts
+++ src/request/index.ts
@@ -5,8 +5,21 @@ import { storeToRefs } from 'pinia'
 const { useInfoStore } = useStore()
 const { token } = storeToRefs(useInfoStore)
 
-export let baseUrl: string = ''; // 当为空时会判断是否是生产环境，否则就是写死的
+export let baseUrl: string = ''; // 当要动态配置时，为空时就好了，在下方环境判断中配置。只支持两种环境，即开发和生产环境。
 !baseUrl && (baseUrl = process.env.NODE_ENV === "development" ? "http://test.chuangjisu.com" : "https://ec.zjpuyule.com")
+
+// 运行平台判断
+switch (uni.getSystemInfoSync().platform) {
+	case 'android':
+		console.log('运行Android上');
+		break;
+	case 'ios':
+		console.log('运行iOS上');
+		break;
+	default:
+		console.log('运行在开发者工具或H5上');
+		break;
+}
 
 export const WX_APPID = "wxddf85aa6c5bdc11a";
 
@@ -23,25 +36,36 @@ const config: fetchConfig = {
 const whiteList = ["/captchaImage", "/login"];
 
 function beforeRequestFun(newConfig: fetchConfig) {
-	newConfig.url = baseUrl + newConfig.url;
+	if (newConfig.url && !newConfig.url.includes('http')) {
+		newConfig.url = baseUrl + newConfig.url
+	}
+
 	if (!whiteList.some((item) => newConfig.url && newConfig.url.includes(item))) {
 		newConfig.header = { ...newConfig.header, Authorization: token || "" };
 	}
+
+	uni.showLoading({
+		title: '加载中'
+	})
+
 	return newConfig;
 }
 
-function afterRequestFun(result: any): any {
-	const { data } = result
+function afterRequestFun({ data }: any): any {
+	uni.hideLoading();
 	if (data.code === 200) {
 		return data;
 	} else if (data.code === 401) {
 		uni.$tm.u.toast('身份过期')
-		return Promise.reject(data)
+		uni.reLaunch({
+			url: 'pages/login/index'
+		});
 	} else if (data.code >= 500) {
 		uni.$tm.u.toast(data.msg || '系统繁忙，请稍后再试')
 		return Promise.reject(data)
 	} else {
-		return Promise.reject('未捕捉的错误')
+		uni.$tm.u.toast('未捕捉的错误！')
+		return Promise.reject(data)
 	}
 }
 
